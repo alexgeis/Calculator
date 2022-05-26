9442da9fd21938261bddbdabed1bf5746d961e62
Alex Geis <siegxela@gmail.com>
2022-05-25 20:44:33 -0600

    updateOperand and updateOperator functions added to JS

--- script.js
+++ script.js
@@ -32,25 +32,31 @@ const twoBtn = document.querySelector("#two-btn");
 const oneBtn = document.querySelector("#one-btn");
 const zeroBtn = document.querySelector("#zero-btn");
 
-clearBtn.onclick = () => clearDisplay();
-plusMinusBtn.onclick = () => posNegOperator();
-moduloBtn.onclick = () => addModuloOperator();
-divideBtn.onclick = () => addDivideOperator();
-multiplyBtn.onclick = () => addMultiplyOperator();
-minusBtn.onclick = () => addMinusOperator();
-plusBtn.onclick = () => addPlusOperator();
-decimalBtn.onclick = () => addDecimal();
-equalBtn.onclick = () => evaluate();
-//NUMBERS
-nineBtn.onclick = (e) => enter9(e.target.value);
-eightBtn.onclick = (e) => enter8(e.target.value);
-sevenBtn.onclick = (e) => enter7(e.target.value);
-sixBtn.onclick = (e) => enter6(e.target.value);
-fiveBtn.onclick = (e) => enter5(e.target.value);
-fourBtn.onclick = (e) => enter4(e.target.value);
-threeBtn.onclick = (e) => enter3(e.target.value);
-twoBtn.onclick = (e) => enter2(e.target.value);
-oneBtn.onclick = (e) => enter1(e.target.value);
+document.querySelectorAll(".digit-btn").forEach((item) => {
+	item.addEventListener("click", (event) => {
+		updateOperands(item.textContent);
+	});
+});
+
+// clearBtn.onclick = () => clearDisplay();
+// plusMinusBtn.onclick = () => posNegOperator();
+// moduloBtn.onclick = () => addModuloOperator();
+// divideBtn.onclick = () => addDivideOperator();
+// multiplyBtn.onclick = () => addMultiplyOperator();
+// minusBtn.onclick = () => addMinusOperator();
+// plusBtn.onclick = () => addPlusOperator();
+// decimalBtn.onclick = () => addDecimal();
+// equalBtn.onclick = () => evaluate();
+// //NUMBERS
+// nineBtn.onclick = (e) => enter9(e.target.value);
+// eightBtn.onclick = (e) => enter8(e.target.value);
+// sevenBtn.onclick = (e) => enter7(e.target.value);
+// sixBtn.onclick = (e) => enter6(e.target.value);
+// fiveBtn.onclick = (e) => enter5(e.target.value);
+// fourBtn.onclick = (e) => enter4(e.target.value);
+// threeBtn.onclick = (e) => enter3(e.target.value);
+// twoBtn.onclick = (e) => enter2(e.target.value);
+// oneBtn.onclick = (e) => enter1(e.target.value);
 
 //Math functions
 function add(a, b) {
@@ -90,6 +96,57 @@ function operate(operator, firstArg, secondArg) {
 			break;
 	}
 }
+
+const firstOperandSpan = document.getElementById("firstOperand");
+const operatorSpan = document.getElementById("operator");
+const secondOperandSpan = document.getElementById("secondOperand");
+
+function updateOperands(value) {
+	if (operatorSpan.textContent == "") {
+		firstOperandSpan.textContent += value;
+	} else {
+		secondOperandSpan.textContent += value;
+	}
+}
+
+document.querySelectorAll(".operator-btn").forEach((item) => {
+	item.addEventListener("click", (event) => {
+		updateOperator(item.textContent);
+	});
+});
+
+function updateOperator(value) {
+	if (
+		firstOperandSpan.textContent != "" &&
+		secondOperandSpan.textContent == ""
+	) {
+		operatorSpan.textContent = value;
+	} else if (
+		firstOperandSpan.textContent != "" &&
+		secondOperandSpan.textContent != ""
+	) {
+		let meanwhile = roundTo5decimalsMax(
+			operate(
+				operatorSpan.textContent,
+				firstOperandSpan.textContent,
+				secondOperandSpan.textContent
+			)
+		);
+		firstOperandSpan.textContent = meanwhile;
+		answerZone.textContent = meanwhile;
+		operatorSpan.textContent = value;
+		secondOperandSpan.textContent = "";
+		checkIfEvilAll();
+	} else if (
+		firstOperandSpan.textContent == "" &&
+		answerZone.textContent != ""
+	) {
+		firstOperandSpan.textContent = answerZone.textContent;
+		operatorSpan.textContent = value;
+		checkIfEvilAll();
+	}
+}
+
 // TO USE FOR KEYBOARD INPUT
 // document.body.onkeydown = (e) => setCurrentKey(e.key);
 
